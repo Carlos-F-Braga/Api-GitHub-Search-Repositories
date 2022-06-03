6f3b461211d3721be6c0d036d81b9b3ced9cbb8a
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-06-03 08:09:11 -0300

    Styles edition, removing old files and Icon changes

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,31 +1,16 @@
-import { FaTwitter, FaFacebook, FaLinkedin, FaInstagram, FaYoutube, FaGithub } from 'react-icons/fa';
+import { FaLinkedin, FaYoutube, FaGithub } from 'react-icons/fa';
 import { StyledSocialIcons } from './styles/SocialIcons.styled';
 
 export default function SocialIcons() {
     return (
         <StyledSocialIcons>
-          <li>
-            <a href="https://twitter.com">
-                <FaTwitter/>    
-            </a>
-        </li>
-        <li>
-            <a href="https://facebook.com">
-                <FaFacebook/>    
-            </a>
-        </li>
         <li>
             <a href="https://linkedin.com">
                 <FaLinkedin/>    
             </a>
         </li>
         <li>
-            <a href="https://instagram.com">
-                <FaInstagram/>    
-            </a>
-        </li>
-        <li>
-            <a href="https://youtube.com">
+            <a href="https://github.com/youtube">
                 <FaYoutube/>    
             </a>
         </li>
